726001b082490b192db2ff0583570aa9302f0fae
Derek <derekdvalerio@gmail.com>
2025-11-07 20:16:11 +0800

    feat: set method

--- hashmap.js
+++ hashmap.js
@@ -1,33 +1,36 @@
-
-function hash(key) {
-
-  let hashCode = 0;
-
-  const primeNumber = 31;
-  for (let i = 0; i < key.length; i++) {
-    hashCode = primeNumber * hashCode + key.charCodeAt(i);
+class HashMap {
+  constructor(loadFactor = 0.75, initialCapacity = 16) {
+    this.loadFactor = loadFactor;
+    this.capacity = initialCapacity;
+    this.buckets = new Array(initialCapacity).fill(null).map(() => []);
   }
 
-  return hashCode;
-} 
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
 
+    return hashCode % this.capacity;
+  }
 
+  set(key, value) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
 
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket[i][1] = value;
+        return;
+      }
+    }
 
+    bucket.push([key, value]);
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  get(key) {
+    const index = this.hash(key);
+    return this.buckets[index];
+  }
+}
